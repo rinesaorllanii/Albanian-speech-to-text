cb73869508bdd7150fbb0c06eccd8918b92de4a9
laurentkadrijaj1 <kadrijajlaurent5@gmail.com>
2025-12-18 21:40:18 +0100

    Transcription Session Factory

--- components/transcriptionSessionFactory.py
+++ components/transcriptionSessionFactory.py
@@ -2,17 +2,33 @@ from components.dialect_management import DialectManagement
 from components.mode import Mode
 from components.singleSession import SingleSession
 from components.collaborationSession import CollaborationSession
-from components.transcriptionSession import TranscriptionSession
 
-class TranscriptionSessionFactory:
+class TranscriptionSessionFactory: #Single Factory 
+#     @staticmethod
+#     def create_transcription_session(self, mode: Mode, dialect_manager: DialectManagement, user_id=None, admin_id=None):
+        # if admin_id is not None:
+        #     return CollaborationSession(mode, admin_id, dialect_manager)
+        # elif user_id is not None:
+        #     return SingleSession(mode, user_id, dialect_manager)         
+
+    session_creation_map = {
+        (True, False): CollaborationSession,
+        (False, True): SingleSession
+    }
+
     @staticmethod
-    def create_transcription_session(self, mode: Mode, user_id: int, dialect_manager: DialectManagement):
-        if mode == Mode.SINGLE:
-            return SingleSession(mode, user_id, dialect_manager)
-        elif mode == Mode.COLLABORATION:
-            return CollaborationSession(mode, user_id, dialect_manager)
-            
-        elif mode == Mode.OFFLINE:
-            return SingleSession(mode, user_id)
-        else:
-            raise ValueError("Invalid mode provided. Supported modes are ONLINE and OFFLINE.")
+    def _get_session_type(is_admin, is_user):
+        # Lazy loading
+        return TranscriptionSessionFactory.session_creation_map.get((is_admin, is_user), None)
+
+    @staticmethod
+    def create_transcription_session(mode: Mode, dialect_manager: DialectManagement, user_id=None, admin_id=None):
+        is_admin = admin_id is not None
+        is_user = user_id is not None
+
+        # Lazy loading
+        session_type = TranscriptionSessionFactory._get_session_type(is_admin, is_user)
+
+        if session_type:
+            # RIP
+            return session_type(mode, admin_id or user_id, dialect_manager)
